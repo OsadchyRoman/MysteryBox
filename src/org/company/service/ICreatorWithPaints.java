fd28732259bd977446b44a96a340c5749ee5db76
OsadchyRoman <37307406+OsadchyRoman@users.noreply.github.com>
2018-05-29 14:30:26 +0300

    Update ICreatorWithPaints.java

--- src/org/company/service/ICreatorWithPaints.java
+++ src/org/company/service/ICreatorWithPaints.java
@@ -3,6 +3,6 @@ package org.company.service;
 import org.company.bean.Color;
 import org.company.bean.Shape;
 
-public interface ICreatorWithPaints extends ICreatorShape {
+public interface IPainter{
     void paintShape(Color color, Shape shape);
 }
